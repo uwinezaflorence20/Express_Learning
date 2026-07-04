5a48494b05a293177b3aa7d9c0e4f6c288a3ce89
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-07-04 19:34:35 +0200

    get request mastered

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const app = express(); // this is the server
+app.listen(3000,()=>{
+    console.log("server running on 3000 port ")
+});
+
+app.set('view engine','ejs')
+
+// creating the GET route
+app.get("/",(req,res)=>{
+    res.send("This is the responses");
+})
+
+app.get("/web", (req, res) => {
+    res.render("Home");
+});
+
+
+//getting the users by the id
+app.get("/users/:id", (req, res) => {
+
+    console.log(req.params);
+
+    res.send("User Found");
+
+});
 
