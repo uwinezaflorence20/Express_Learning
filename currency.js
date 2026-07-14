60efb9ac74e42dd9036548c813c9fd0e624bdc05
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-07-14 11:02:34 +0200

    done well

---
+++ currency.js
@@ -0,0 +1,53 @@
+// Build an Express server with a single GET endpoint /convert.
+
+// Accept two query parameters:
+// amount (numeric)
+// currency (either "usd", "eur", or "gbp")
+// The server should convert the given amount to Rwandan Francs (RWF) using predefined conversion rates stored in a JavaScript object.
+// Respond with a JSON object showing the input and the converted value:
+
+//   {
+//     "input": { "amount": 10, "currency": "usd" },
+//     "convertedAmount": 13000,
+//     "unit": "RWF"
+//   }
+
+
+// Handle missing or invalid parameters with a 400 response and descriptive error.
+
+const express = require('express');
+const app = express();
+let rates = {
+    usd: 1500,
+    eur: 1200,
+    gbp: 1000
+}
+
+app.post('/convert', (req, res) => {
+    let { amount, currency } = req.body;
+    if (!amount || !currency) {
+        return res.status(400).json({ error: "amount and currency are required" });
+    }
+    let value = Number(amount);
+    if (isNaN(value)) {
+        return res.status(400).json({error: "The amount should be a numbers"});
+    }
+
+    if(!value[currency]){
+        return res.status(400).json({error:"The currency should be usd,eur and gbp"});
+    }
+    let convertedValue = value * value[currency];
+
+    res.json({
+        input:{
+            amount:value,
+            currency:currency
+        },
+        convertedValue:convertedValue,
+        unit:"RWF"
+    })
+})
+
+app.listen(5000,()=>{
+    console.log("The server is running locally on 5000 port");
+})
